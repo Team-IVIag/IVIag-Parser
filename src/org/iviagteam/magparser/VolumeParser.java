d72c69e67687715a01490cb5fe5d4b3fa4f1fce8
HelloWorld017 <deu07115@gmail.com>
2016-02-22 03:15:41 +0900

    Updated getStatus

--- src/org/iviagteam/magparser/VolumeParser.java
+++ src/org/iviagteam/magparser/VolumeParser.java
@@ -2,10 +2,12 @@ package org.iviagteam.magparser;
 
 import org.iviagteam.magparser.callback.VolumeCallback;
 
-public class VolumeParser extends Thread{
+public abstract class VolumeParser extends Thread{
 	public enum Status{IDLE, CONNECTING, TITLE_PARSING, IMG_PARSING, VOLUME_PARSING, DONE};
 	
 	public VolumeParser(String url, VolumeCallback callback) {
 		
 	}
+	
+	public abstract Status getStatus();
 }
